24fd522f8618ad75c74e42112ee3df29011b2c73
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-07-20 08:00:32 -0400

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+words = ['enlists', 'google', 'inlets', 'banana']
+
+
+class Anagram:
+    def __init__(self, word=""):
+        self.word = word
+
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        # Find the anagrams by comparing sorted versions of the word
+        return [w for w in words if sorted(w) == sorted_word]
+
+
+sample = Anagram("listen")
+print(sample.match(words))
